ea495b94c8ccbfc47789c809f4a41e8666b862e8
augustinlss <augustin.zhlass@gmail.com>
2023-10-14 18:07:55 +0200

    Fixed production issue

--- server/controllers/data.controller.js
+++ server/controllers/data.controller.js
@@ -13,21 +13,21 @@ router.route('/').get()
 router.post(("/data"), async (req, res)=>{
     try{
       const _id = "65295cc65376bbb8c1d428fe"
-      const user = UserData.findOne({_id});
+      const user = await UserData.findOne({_id});
 
-      user.lastKnowLocation = req.body.location;
-      const distance = await haversineDistance(lastKnowLocation[0], lastKnowLocation[1], req.body.location[0], req.body.location[1])
+      const distance = await haversineDistance(user.lastKnownLocationLat, user.lastKnownLocationLong, req.body.locationLAT, req.body.locationLONG)
+      user.lastKnownLocationLat = req.body.locationLAT;
+      user.lastKnownLocationLong = req.body.locationLONG;
       //51.437233, 5.446286 long lat of limit west
       //51.439345, 5.507008 long lat of limit east
       //51.454211, 5.476489 long lat of limit north
       //51.420886, 5.481924 long lat of limit south
       //51.439475, 5.477845 long lat of center
-      if(req.body.location[0] > 51.454211 || req.body.location[0] < 51.420886 ||
-        req.body.location[1] > 5.507008 || req.body.location[1] < 5.446286) {
-          user.distanceNDrivenPol = user.distanceDrivenNPol + distance
-      }
-      if(req.body.location[0] < 51.454211 || req.body.location[0] > 51.420886 ||
-        req.body.location[1] < 5.507008 || req.body.location[1] > 5.446286) {
+      console.log(distance);
+      if(req.body.locationLAT > 51.454211 || req.body.locationLAT < 51.420886 ||
+        req.body.locationLONG > 5.507008 || req.body.locationLONG < 5.446286) {
+          user.distanceDrivenNPol = user.distanceDrivenNPol + distance
+      }else {
           user.distanceDrivenPol = user.distanceDrivenPol + distance
       }
       user.save().then(user => res.json(user)).catch(err => res.status(400).json("Error! " + err));
@@ -38,16 +38,27 @@ router.post(("/data"), async (req, res)=>{
     }
 });
 
-router.post(("/startSession"), (req, res) => {
+router.post(("/startSession"), async (req, res) => {
   try{
     const _id = "65295cc65376bbb8c1d428fe"
-    const user = UserData.findOne({_id});
-    user.lastKnowLocation = req.body.location;
+    const user = await UserData.findOne({_id}); 
+    user.lastKnownLocationLat = req.body.locationLAT;
+    user.lastKnownLocationLong = req.body.locationLONG;
     user.save().then(user => res.json(user)).catch(err => res.status(400).json("Error! " + err));
   }catch (e) {
     return res.json("Error: "+ e.message);
   }
 });
+
+router.get(("/info"), async (req,res) => {
+  try{
+    const _id = "65295cc65376bbb8c1d428fe"
+    const user = await UserData.findOne({_id}); 
+    return res.json(user);
+  }catch (e) {
+    return res.send(e);
+  }
+})
 
 
 module.exports = router
